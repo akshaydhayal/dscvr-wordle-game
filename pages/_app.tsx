b754e5b4f82a802bf23fc54ecd7a07b4e144dc42
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-30 20:47:08 +0530

    Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Worldle App</title>
+        <meta name="dscvr:canvas:version" content="vNext" />
+        <meta name="og:image" content="https://my-canvas.com/preview-image.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
